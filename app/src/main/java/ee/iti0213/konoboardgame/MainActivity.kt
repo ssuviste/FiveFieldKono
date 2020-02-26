d3f2c6cc4bc97877f9aa2d4b17dfcb38a5b30ae6
Siim Suviste <siim.suviste@ttu.ee>
2020-02-26 06:27:52 +0200

    UI Construction

--- app/src/main/java/ee/iti0213/konoboardgame/MainActivity.kt
+++ app/src/main/java/ee/iti0213/konoboardgame/MainActivity.kt
@@ -1,12 +1,31 @@
 package ee.iti0213.konoboardgame
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun boardButtonOnClick(view: View) {
+        val buttonId = findViewById<Button>(view.id)
+        when (buttonId.backgroundTintList) {
+            getColorStateList(R.color.colorBlue) ->
+                buttonId.backgroundTintList = getColorStateList(R.color.colorBlueHighlighted)
+            getColorStateList(R.color.colorBlueHighlighted) ->
+                buttonId.backgroundTintList = getColorStateList(R.color.colorBlue)
+            getColorStateList(R.color.colorOrange) ->
+                buttonId.backgroundTintList = getColorStateList(R.color.colorOrangeHighlighted)
+            getColorStateList(R.color.colorOrangeHighlighted) ->
+                buttonId.backgroundTintList = getColorStateList(R.color.colorOrange)
+        }
     }
 }
