e7feba4bc08d1852910054393d61f852716027b9
Siim Suviste <siim.suviste@ttu.ee>
2020-03-01 06:03:48 +0200

    UI Construction

--- app/src/main/java/ee/iti0213/konoboardgame/MainActivity.kt
+++ app/src/main/java/ee/iti0213/konoboardgame/MainActivity.kt
@@ -5,16 +5,23 @@ import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
+import kotlinx.android.synthetic.main.game_stats.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        var moves = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    // Just for testing UI color change
     private fun changeButtonColor(view: View, colorList1: ColorStateList?, colorList2: ColorStateList?) {
         when (ViewCompat.getBackgroundTintList(view)) {
             colorList1 ->
@@ -23,6 +30,30 @@ class MainActivity : AppCompatActivity() {
                 ViewCompat.setBackgroundTintList(view, colorList1)
             else -> return
         }
+    }
+
+    // Just for testing UI color change
+    private fun swapPlayerHighlights() {
+        val orange = ContextCompat.getColor(this, R.color.colorOrange)
+        val orangeH = ContextCompat.getColor(this, R.color.colorOrangeHighlighted)
+        val blue = ContextCompat.getColor(this, R.color.colorBlue)
+        val blueH = ContextCompat.getColor(this, R.color.colorBlueHighlighted)
+        val mode = android.graphics.PorterDuff.Mode.MULTIPLY
+
+        val player1Icon = findViewById<ImageView>(player1Color.id)
+        val player2Icon = findViewById<ImageView>(player2Color.id)
+
+        when (moves % 2) {
+            0 -> {
+                player1Icon.setColorFilter(orangeH, mode)
+                player2Icon.setColorFilter(blue, mode)
+            }
+            1 -> {
+                player1Icon.setColorFilter(orange, mode)
+                player2Icon.setColorFilter(blueH, mode)
+            }
+        }
+        moves++
     }
 
     fun buttonBoardOnClick(view: View) {
@@ -35,6 +66,8 @@ class MainActivity : AppCompatActivity() {
             blue, blueH -> changeButtonColor(view, blue, blueH)
             orange, orangeH -> changeButtonColor(view, orange, orangeH)
         }
+
+        swapPlayerHighlights()
     }
 
     fun buttonHelpOnClick(view: View) {
